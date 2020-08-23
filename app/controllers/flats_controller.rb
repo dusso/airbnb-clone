bb6690a17f79b08c8f89ee6cd58d7672f3b3df1c
Denis Dusso <dddusso@gmail.com>
2020-08-23 17:58:23 +0200

    added show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,9 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all # vai retornar um array de hash
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
 end
